8c24da7fd251cff2f682af1e191e700c74521327
MUKUL-RAJPUT2004 <mukulrajput200705@gmail.com>
2025-09-29 23:19:45 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher([
+    "/signin",
+    "/signup",
+    "/",
+    "/home"
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+export default clerkMiddleware(async (auth, req)=>{
+    const { userId } = await auth();
+    const currentUrl = new URL(req.url);
+    const isAccessingDashboard = currentUrl.pathname === "/home";
+    const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+    if(userId && isPublicRoute(req) && !isAccessingDashboard){
+        return NextResponse.redirect(new URL("/home", req.url))
+    }
+
+    //not logged in
+    if(!isPublicApiRoute(req) && !isPublicApiRoute(req)){
+        return NextResponse.redirect(new URL("/signin", req.url))
+    }
+
+    if(isApiRequest && isPublicApiRoute(req)){
+        return NextResponse.redirect(new URL("/signin", req.url))
+    }
+
+    return NextResponse.next();
+
+})
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(.*)"],
+};
